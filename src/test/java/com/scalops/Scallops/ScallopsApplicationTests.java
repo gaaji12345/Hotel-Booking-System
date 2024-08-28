3203f77cd48de6a3f44437137aa3735c4b7cc0b5
gaji12345 <gajimax12345@gmail.com>
2024-08-28 21:33:26 +0530

    test

---
+++ src/test/java/com/scalops/Scallops/ScallopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scalops.Scallops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScallopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
