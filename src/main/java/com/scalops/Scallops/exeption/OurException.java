7052eb7dfd503ad5a39f474c707d8ab39916aafa
gaji12345 <gajimax12345@gmail.com>
2024-08-30 19:22:05 +0530

    create exeption class

---
+++ src/main/java/com/scalops/Scallops/exeption/OurException.java
@@ -0,0 +1,11 @@
+package com.scalops.Scallops.exeption;/*  gaajiCode
+    99
+    30/08/2024
+    */
+
+public class OurException extends RuntimeException{
+
+    public OurException(String message){
+        super(message);
+    }
+}
